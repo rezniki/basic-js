79dd25c5d92b62cf49ede1f1316aff720a2ae1b4
rezniki <igorreznik2001@gmail.com>
2023-07-10 23:52:17 +0300

    Add new variable in app.js

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ let surname = 'Ivanov'
 let age = 21
 let nationality = 'Belarusian'
 
+// const number = 5
+// const string = 'Igor'
+// const floor = 'Male'  
+
+// number = 6
+// string = 'Darya'
+// floor = 'Female'
+
+job = 'Backend-developer'
+salary = 1000
+city = 'Los-Angeles'
+
 console.log(name)
 console.log(surname)
 console.log(age)
 console.log(nationality)
+
+console.log(job)
+console.log(salary)
+console.log(city)
+
+// console.log(number)
+// console.log(string)
+// console.log(floor)
