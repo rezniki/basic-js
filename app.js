c92f68229e49cc8bf60c898037e46555bdb321dd
rezniki <igorreznik2001@gmail.com>
2023-07-11 22:25:03 +0300

    Add file app.js type: let

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-name = 'Igor'
-age = 21
-eye = 'gray-green'
-job = 'Frotend-developer'
+let name = 'Igor';
+let age = 21;
+let eye = 'gray-green';
+let job = 'Frotend-developer';
 
-console.log(typeof(name))
-console.log(typeof(age))
-console.log(typeof(eye))
-console.log(typeof(job))
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(eye));
+console.log(typeof(job));
 
 let man = {
     name: 'Igor',
@@ -15,8 +15,8 @@ let man = {
     job: 'Frotend-developer',
 }
 
-console.log(typeof(man))
+console.log(typeof(man));
 
-let myElement = document.querySelector(".task__container")
+let myElement = document.querySelector(".task__container");
 
-console.log(myElement)
+console.log(myElement);
