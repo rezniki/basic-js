3dc5f147bf602a023368b4a07a314e8b7a420042
rezniki <igorreznik2001@gmail.com>
2023-07-14 14:48:30 +0300

    Created button for add task in index.html

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ let car = {
     speed: 300,
     stamp: 'Volkswagen',
     color: 'white',
-}
+};
 
 let dog = {
     breed: 'Hachiko',
     height: 71,
     weight: 50,
     country: 'Japan',
-}
+};
 
 dog.breed = 'Sheepdog';
 dog.height = 65;
@@ -19,6 +19,24 @@ dog.country = 'Germany';
 
 console.log(car);
 console.log(dog);
+
+let sumNumbers = function(numberA, numberB) {
+    console.log(numberA + numberB);
+};
+
+sumNumbers(12, 15);
+
+let helloWorld = function() {
+    console.log('Hello world)');
+};
+
+helloWorld();
+
+let sayHello = function(word, name) {
+    console.log(`Приветствую ${word}, ${name}`);
+};
+
+sayHello('вас', 'Игорь');
 
 let mainElement = document.querySelector('.task');
 let myElement = document.querySelector('.task__container');
@@ -30,6 +48,13 @@ let cardElement = document.querySelector('.task__card');
 let avatarElement = document.querySelector('.task__avatar');
 let nameElement = document.querySelector('.task__name');
 
+let buttonElement = document.querySelector('.task__button');
+
+buttonElement.onclick = function() {
+    console.log('Произошел клик');
+    taskElement.style.color = 'white';
+};
+
 console.log(mainElement);
 console.log(myElement);
 console.log(taskElement);
@@ -39,22 +64,3 @@ console.log(contentElement);
 console.log(cardElement);
 console.log(avatarElement);
 console.log(nameElement);
-
-let sumNumbers = function(numberA, numberB) {
-    console.log(numberA + numberB);
-}
-
-sumNumbers(12, 15);
-
-let helloWorld = function() {
-    console.log('Hello world)');
-}
-
-helloWorld();
-
-let sayHello = function(word, name) {
-    console.log(`Приветствую ${word}, ${name}`);
-}
-
-sayHello('вас', 'Игорь');
-
