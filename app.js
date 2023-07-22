4425eec44289ec590314a11342211d2c1543ffaf
rezniki <igorreznik2001@gmail.com>
2023-07-22 14:55:56 +0300

    Add event onclick for element: myCard

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let productContents = document.querySelectorAll('.product__content');
 const funcContent = function(myCard) {
     console.log(myCard);
     myCard.style.background = 'red';
+    myCard.onclick = function() {
+        console.log('Привет');
+    }
 }
 
 productContents.forEach(funcContent);
